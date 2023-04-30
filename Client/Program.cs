54bff48adaab734b53f7cf29bf40c679fba8bed4
Blooife <beusas.23@gmail.com>
2023-04-30 18:02:32 +0300

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+using System.Net.Http;
+
+
+
+        string serverURL = "http://localhost:"; 
+        StreamWriter outStream;
+        Console.WriteLine("Enter port:");
+        string port = Console.ReadLine();
+        serverURL += port + "/";
+        var client = new HttpClient();
+        
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("\nВыберите запрос:");
+                Console.WriteLine("1.Get 2.Put 3.Post 4.Delete 5.Copy 6.Move 0.Exit");
+
+                switch (int.Parse(Console.ReadLine()))
+                    {
+                        case 1:
+                        {
+                            SendGet();
+                            break;
+                        }
+                        case 2:
+                        {
+                            SendPut();
+                            break;
+                        }
+                        case 3:
+                        {
+                            SendPost();
+                            break;
+                        }
+                        case 4:
+                        {
+                            SendDelete();
+                            break;
+                        }
+                        case 5:
+                        {
+                            SendCopy();
+                            break;
+                        }
+                        case 6:
+                        {
+                            SendMove();
+                            break;
+                        }
+                        case 0:
+                        {
+                            Environment.Exit(0);
+                            break;
+                        }
+                    }
+            }
+            catch (Exception e) 
+            {
+                    Console.WriteLine(e.Message);
+            }
+        }
+
+        async void sendGetAsync()
+        {
+            Console.WriteLine("Enter filename");
+            string filename = Console.ReadLine();
+            string url = serverURL + filename;
+            HttpMethod method = new HttpMethod("GET");
+            var msg = new HttpRequestMessage(method, url);
+            StringContent str = new StringContent(filename);
+            HttpResponseMessage response =  await client.SendAsync(msg);
+            if (response.IsSuccessStatusCode)
+            {
+                var s = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(s);
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+            
+        }
+
+        void SendGet()
+        {
+            Console.WriteLine("Enter filename to read");
+            string filename = Console.ReadLine();
+            string url = serverURL + filename;
+            HttpMethod method = new HttpMethod("GET");
+            var msg = new HttpRequestMessage(method, url);
+            HttpResponseMessage response =  client.Send(msg);
+            if (response.IsSuccessStatusCode)
+            {
+                StreamReader streamReader = new StreamReader(response.Content.ReadAsStream());
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            else
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+        }
+
+        void SendDelete()
+        {
+            Console.WriteLine("Enter filename to delete:");
+            string filename = Console.ReadLine();
+            string url = serverURL + filename;
+            HttpMethod method = new HttpMethod("DELETE");
+            var msg = new HttpRequestMessage(method, url);
+            HttpResponseMessage response =  client.Send(msg);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+        }
+
+        void SendPut()
+        {
+            Console.WriteLine("Enter filename to rewrite:");
+            string filename = Console.ReadLine();
+            string url = serverURL + filename;
+            HttpMethod method = new HttpMethod("PUT");
+            var msg = new HttpRequestMessage(method, url);
+            Console.WriteLine("Enter new content of the file");
+            string data = Console.ReadLine();
+            msg.Content = new StringContent(data);
+            HttpResponseMessage response =  client.Send(msg);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+        }
+        
+        void SendPost()
+        {
+            Console.WriteLine("Enter filename to add:");
+            string filename = Console.ReadLine();
+            string url = serverURL + filename;
+            HttpMethod method = new HttpMethod("POST");
+            var msg = new HttpRequestMessage(method, url);
+            Console.WriteLine("Enter content to add");
+            Stream? inputStream = null;
+            string data = Console.ReadLine();
+            msg.Content = new StringContent(data);
+            HttpResponseMessage response =  client.Send(msg);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+            }
+        }
+
+        void SendCopy()
+        {
+            Console.WriteLine("Enter filename to copy:");
+            string filename = Console.ReadLine();
+            string url = serverURL + filename;
+            HttpMethod method = new HttpMethod("COPY");
+            var msg = new HttpRequestMessage(method, url);
+            Console.WriteLine("Enter ditectory to copy");
+            string data = Console.ReadLine();
+            msg.Content = new StringContent(data);
+            HttpResponseMessage response =  client.Send(msg);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+                StreamReader streamReader = new StreamReader(response.Content.ReadAsStream());
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            else
+            {
+                StreamReader streamReader = new StreamReader(response.Content.ReadAsStream());
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+
+        }
+
+        void SendMove()
+        {
+            Console.WriteLine("Enter filename to move:");
+            string filename = Console.ReadLine();
+            string url = serverURL + filename;
+            HttpMethod method = new HttpMethod("MOVE");
+            var msg = new HttpRequestMessage(method, url);
+            Console.WriteLine("Enter ditectory to move");
+            string data = Console.ReadLine();
+            msg.Content = new StringContent(data);
+            HttpResponseMessage response =  client.Send(msg);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Error: " + response.StatusCode);
+                StreamReader streamReader = new StreamReader(response.Content.ReadAsStream());
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+            else
+            {
+                StreamReader streamReader = new StreamReader(response.Content.ReadAsStream());
+                Console.WriteLine(streamReader.ReadToEnd());
+                streamReader.Close();
+            }
+
+        }
+
+    
---
+++ httpS/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Linq;
+using System.Net;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace HttpServer
+{
+    public class Server
+    {
+        private static string IP_ADDRESS = "";
+        private static string dir = "C:/Users/Asus/RiderProjects/httpS/httpS/fileStorage/";
+        private static int status = 200;
+        private static byte[] response = new byte[0];
+
+        public static void Main(string[] args)
+        {
+            bool ok = false;
+            while (!ok)
+            {
+                try
+                {
+                    Console.WriteLine("Enter port number");
+                    int port = Int32.Parse(Console.ReadLine());
+                    HttpListener server = new HttpListener();
+                    IP_ADDRESS = "http://localhost:" + port + "/";
+                    server.Prefixes.Add(IP_ADDRESS);
+                    ok = true;
+                    server.Start();
+                    Console.WriteLine("Server started");
+                    while (true)
+                    {
+                       // var context = await server.GetContextAsync();
+                        HttpListenerContext context = server.GetContext();
+                        Console.WriteLine(context.Request.Url);
+                        HttpListenerRequest request = context.Request;
+                        HttpListenerResponse response = context.Response;
+                        
+                        var hand = new MyHandler();
+                        hand.Handle(context);
+                    }
+                    server.Close();
+                    server.Stop();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong input");
+                }
+                catch (HttpListenerException)
+                {
+                    Console.WriteLine("Port is already in use");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+        }
+
+        public class MyHandler
+        {
+            public void Handle(HttpListenerContext context)
+            {
+                string requestURI = context.Request.Url.ToString();
+                Console.WriteLine(context.Request.HttpMethod + " " + requestURI + " " +
+                                  context.Request.ProtocolVersion);
+                var headers = context.Request.Headers;
+                foreach (string key in headers.Keys)
+                {
+                    Console.WriteLine(key + " : " + headers[key]);
+                }
+
+                //string f = context.Request.
+                string filename = requestURI.Substring(IP_ADDRESS.Length);
+                Console.WriteLine(filename);
+                try
+                {
+                    switch (context.Request.HttpMethod)
+                    {
+
+                        // чтение файла
+                        case "GET":
+                            doGet(filename);
+                            break;
+
+                        // добавление в конец файла
+                        case "POST":
+                            doPost(context.Request, filename);
+                            break;
+
+                        // перезапись файла
+                        case "PUT":
+                            doPut(context.Request, filename);
+                            break;
+
+                        // удаление файла
+                        case "DELETE":
+                            doDelete(filename);
+                            break;
+
+                        // перемещение файла
+                        case "MOVE":
+                            doMove(context, filename);
+                            break;
+
+                        // копирование файла
+                        case "COPY":
+                            doCopy(context, filename);
+                            break;
+
+                        default:
+                            context.Response.StatusCode = 405;
+                            break;
+                    }
+                }
+                catch (FileNotFoundException ex)
+                {
+                    context.Response.StatusCode = 404;
+                }
+                catch (IOException ex)
+                {
+                    context.Response.StatusCode = 400;
+                }
+                context.Response.ContentLength64 = response.Length;
+                context.Response.OutputStream.Write(response, 0, response.Length);
+                context.Response.OutputStream.Close();
+                Array.Resize(ref response,0);
+            }
+
+            private static void doGet(string filename)
+            {
+                response = File.ReadAllBytes(dir + filename);
+            }
+
+            private static void doPut(HttpListenerRequest request, string filename)
+            {
+                
+                Stream s = request.InputStream;
+                FileStream fileStream = new FileStream(dir+filename, FileMode.Truncate, FileAccess.Write);
+                BinaryWriter writer = new BinaryWriter(fileStream);
+                byte[] buffer = new byte[1024];
+                int bytesRead = s.Read(buffer, 0, buffer.Length);
+                while (bytesRead > 0)
+                {
+                    writer.Write(buffer, 0, bytesRead);
+                    bytesRead = s.Read(buffer, 0, buffer.Length);
+                }
+                writer.Close();
+                fileStream.Close();
+                s.Close();
+            }
+            
+            private static void doPost(HttpListenerRequest request, string filename)
+            {
+                Stream s = request.InputStream;
+                StreamReader sr = new StreamReader(s);
+                string str = sr.ReadToEnd();
+                if (File.Exists(dir + filename))
+                {
+                    File.AppendAllText(dir+filename, str);
+                }
+                else
+                {
+                   // File.Create(dir + filename);
+                    File.WriteAllText(dir+filename,str);
+                }
+                sr.Close();
+                s.Close();
+            }
+
+
+            private static void doMove(HttpListenerContext context, string filename)
+            {
+                Stream s = context.Request.InputStream;
+                StreamReader sr = new StreamReader(s);
+                string newPath = sr.ReadToEnd();
+                newPath += filename.Substring(filename.LastIndexOf('/')+1);
+                if (File.Exists(dir+newPath))
+                {
+                    response = Encoding.Default.GetBytes("File with such name is already exists");
+                } else
+                if (!File.Exists(dir + filename))
+                {
+                    response = Encoding.Default.GetBytes("There is no such file you want to move");
+                }
+                else
+                {
+                    File.Copy(dir+filename, dir+newPath);
+                    File.Delete(dir+filename);  
+                }
+            }
+
+            private static void doDelete(string filename)
+            {
+                try
+                {
+                    File.Delete(Path.Combine(dir, filename));
+                }
+                catch (DirectoryNotFoundException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                
+            }
+           
+            private static void doCopy(HttpListenerContext context, string filename)
+            {
+                Stream s = context.Request.InputStream;
+                StreamReader sr = new StreamReader(s);
+                string newPath = sr.ReadToEnd();
+                newPath += filename.Substring(filename.LastIndexOf('/')+1);
+                if (File.Exists(dir+newPath))
+                {
+                    response = Encoding.Default.GetBytes("File with such name is already exists");
+                } else
+                if (!File.Exists(dir + filename))
+                {
+                    response = Encoding.Default.GetBytes("There is no such file you want to copy");
+                }else
+                    File.Copy(dir+filename, dir+newPath);
+            }
+        }
+    }
+}
